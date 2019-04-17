4c1a15a13c675c44df3a58775433f733752a1c6e
carlyryan <carly6298@gmail.com>
2019-04-17 17:47:02 -0400

    Tried to get rid of null pointer.

--- app/src/main/java/com/example/personalwellness/SurveyActivity.java
+++ app/src/main/java/com/example/personalwellness/SurveyActivity.java
@@ -35,45 +35,45 @@ public class SurveyActivity extends AppCompatActivity {
 
         surveyResponses = new HashMap<Integer, String>();
 
-        mQuestionNumberView = (TextView)findViewById(R.id.question_number);
-        mQuestionView = (TextView)findViewById(R.id.question);
+        mQuestionNumberView = (TextView) findViewById(R.id.question_number);
+        mQuestionView = (TextView) findViewById(R.id.question);
 
-        mButtonChoice1 = (Button)findViewById(R.id.choice1);
-        mButtonChoice2 = (Button)findViewById(R.id.choice2);
-        mButtonChoice3 = (Button)findViewById(R.id.choice3);
-        mPrev = (Button)findViewById(R.id.previous_question);
+        mButtonChoice1 = (Button) findViewById(R.id.choice1);
+        mButtonChoice2 = (Button) findViewById(R.id.choice2);
+        mButtonChoice3 = (Button) findViewById(R.id.choice3);
+        mPrev = (Button) findViewById(R.id.previous_question);
 
         updateQuestion();
 
-        mButtonChoice1.setOnClickListener(new View.OnClickListener(){
+        mButtonChoice1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
+            public void onClick(View view) {
                 surveyResponses.put(mQuestionNumber, mButtonChoice1.getText().toString());
                 updateQuestion();
                 Log.d(TAG, "-----------answer choice 1, question answer: " + mButtonChoice1.getText().toString());
             }
         });
 
-        mButtonChoice2.setOnClickListener(new View.OnClickListener(){
+        mButtonChoice2.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
+            public void onClick(View view) {
                 surveyResponses.put(mQuestionNumber, mButtonChoice2.getText().toString());
                 updateQuestion();
                 Log.d(TAG, "-----------answer choice 2, question answer: " + mButtonChoice2.getText().toString());
             }
         });
 
-        mButtonChoice3.setOnClickListener(new View.OnClickListener(){
+        mButtonChoice3.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
+            public void onClick(View view) {
                 surveyResponses.put(mQuestionNumber, mButtonChoice3.getText().toString());
                 updateQuestion();
                 Log.d(TAG, "-----------answer choice 3, question answer: " + mButtonChoice3.getText().toString());
             }
         });
-        mPrev.setOnClickListener(new View.OnClickListener(){
+        mPrev.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
+            public void onClick(View view) {
                 previousQuestion();
                 Log.d(TAG, "-----------previous question");
             }
@@ -81,7 +81,7 @@ public class SurveyActivity extends AppCompatActivity {
 
     }
 
-    private void updateQuestion(){
+    private void updateQuestion() {
         if (mSurvey.checkQuestionAvailable(mQuestionNumber)) {
             mQuestionView.setText(mSurvey.getQuestion(mQuestionNumber));
             mButtonChoice1.setText(mSurvey.getChoice1(mQuestionNumber));
@@ -89,18 +89,27 @@ public class SurveyActivity extends AppCompatActivity {
             mButtonChoice3.setText(mSurvey.getChoice3(mQuestionNumber));
             mQuestionNumberView.setText(mQuestionNumber + "");
             mQuestionNumber++;
-        }
-        else {
-          User curr = CurrentUser.getCurrentUser(null);
-          //add commands to upadte user with their scores.. carly will do this
-          //add here for the Async stuff !
-
-          Intent i = new Intent(SurveyActivity.this, HomeActivity.class);
-          startActivity(i);
+        } else {
+            User curr = CurrentUser.getCurrentUser(null);
+            //analysis of survey answers
+            //these are dummy values! 
+            int stress = 9;
+            int sleep = 10;
+            int mentalHealth = 5;
+            int community = 3;
+            int ph = 4;
+            curr.updateStress(stress);
+            curr.updateSleep(sleep);
+            curr.updateMentalHealth(mentalHealth);
+            curr.updateCommunity(community);
+            curr.updatePhysicalHealth(ph);
+            //add here for the Async stuff!
+            Intent i = new Intent(SurveyActivity.this, HomeActivity.class);
+            startActivity(i);
         }
     }
 
-    private void previousQuestion(){
+    private void previousQuestion() {
         if (mQuestionNumber > 0) {
             mQuestionNumber--;
             mQuestionNumberView.setText(mQuestionNumber + "");
@@ -111,7 +120,7 @@ public class SurveyActivity extends AppCompatActivity {
         }
     }
 
-    public void createUser(User user){
+    public void createUser(User user) {
 
     }
 }
